aa8e83747529f68fb6fa4031c899bf5750ebf345
SunishPanigrahy <kanhasunish@gmail.com>
2025-09-16 22:38:36 +0530

    added rules

---
+++ models/rule.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// Rules for generating advice
+const ruleSchema = new mongoose.Schema({
+  condition: { type: String, required: true },   // e.g., "soil_type=loamy"
+  advice: { type: String, required: true },      // e.g., "Grow wheat and pulses"
+  created_at: { type: Date, default: Date.now }
+});
+
+const Rule = mongoose.model('Rule', ruleSchema);
+
+module.exports = Rule;
